f51b7bb55f7fa0ad0adcf50c5c5073cd1756cd33
Mário Agostinho <mariojgagostinho@gmail.com>
2024-09-04 16:00:10 +0200

    Inital commit

---
+++ Source/NotCounterStrike/NotCounterStrike.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NotCounterStrike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NotCounterStrike, "NotCounterStrike" );
